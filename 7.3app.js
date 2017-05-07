723932fdd90cf386772c1ee77ceff098ae515b32
VinodCGI <kvinodk4444@gmail.com>
2017-05-07 11:22:36 +0530

    Add files via upload

---
+++ 7.3app.js
@@ -0,0 +1,11 @@
+//Am getting comments(after submitting my assignment) like proper flow is missing. I really didnt get that
+//Please let me know what you are expecting?
+var i=0;
+var counter=0;
+
+//As of now prnting in console until counter increases to 6 times
+while (i<6){
+	counter++;
+	i++;
+	console.log(counter);
+}
